48d89dccc2f819e7a3041de901eb9fc13e654be7
Kevin H <dgkallday98@gmail.com>
2022-01-09 16:30:50 -0500

    Can now save a vinyl album object or a cd album object using album class inheritance

--- PanelMain.java
+++ PanelMain.java
@@ -5,20 +5,29 @@ import java.util.*;
 import java.io.*;
 
 public class PanelMain extends JPanel {
-	ArrayList<Record> collection = new ArrayList<Record>();
+	ArrayList<Album> collection = new ArrayList<Album>();
 	JTextField artField, albField, lenField;
 	JLabel ranAl = new JLabel("");
+	ButtonGroup radioBtnGroup = new ButtonGroup();
 	File file = new File("collection.ser");
 
 	public PanelMain() {
 		getAlbumCollection();
+		JRadioButton vinylBtn = new JRadioButton("Vinyl", true);
+		vinylBtn.setActionCommand("vinyl");
+		JRadioButton cdBtn = new JRadioButton("CD");
+		cdBtn.setActionCommand("cd");
+		radioBtnGroup.add(vinylBtn);
+		radioBtnGroup.add(cdBtn);
 		JLabel artLabel = new JLabel("Artist:");
 		artField = new JTextField(20);
 		JLabel albLabel = new JLabel("Album:");
 		albField = new JTextField(20);
 		JLabel lenLabel = new JLabel("Length:");
 		lenField = new JTextField(10);
-		
+
+		add(vinylBtn);
+		add(cdBtn);
 		add(artLabel);
 		add(artField);
 		add(albLabel);
@@ -36,28 +45,28 @@ public class PanelMain extends JPanel {
 		add(btn);
 		add(ranAl);
 
-		setLayout(new GridLayout(5,2));
+		setLayout(new GridLayout(6,2));
 		btn.addActionListener(new RandomAlbumListener());
 		save.addActionListener(new SaveListener());
 		load.addActionListener(new LoadListener());
 	}
 
 	private void getAlbumCollection() {
+
 		try {
 			ObjectInputStream is = new ObjectInputStream(new FileInputStream(file));
 			collection = (ArrayList) is.readObject();
 
-			System.out.println("Your collection has " + collection.size() + " albums.");
+			System.out.println("Your collection has loaded and has " + collection.size() + " albums.");
 			is.close();
 		} catch (FileNotFoundException e) {
 			System.out.println("File does not exist.");
-			//add file here if you can.
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 
-		for (Record rec : collection) {
-			System.out.println(rec.getAlbumInfo());
+		for (Album r : collection) {
+			System.out.println(r.getAlbumInfo());
 		}
 	}
 
@@ -65,7 +74,8 @@ public class PanelMain extends JPanel {
 		try {
 			ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(file));
 			os.writeObject(collection);
-			System.out.println("Collection is " + collection.size() + " albums big.");
+
+			System.out.println("Collection was serialized and has " + collection.size() + " albums.\n");
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
@@ -82,8 +92,16 @@ public class PanelMain extends JPanel {
 
 	public class SaveListener implements ActionListener {
 		public void actionPerformed(ActionEvent ev) {
-			Record rec = new Record(albField.getText(), artField.getText(), lenField.getText());
-			collection.add(rec);
+
+			if((radioBtnGroup.getSelection().getActionCommand()).equals("vinyl")) {
+				System.out.println("A new vinyl Album was created.");
+				Record rec = new Record(albField.getText(), artField.getText(), lenField.getText());
+				collection.add(rec);
+			} else {
+				System.out.println("A new cd Album was created.");
+				Cd cd = new Cd(albField.getText(), artField.getText(), lenField.getText());
+				collection.add(cd);
+			}
 			saveAlbumCollection();
 			clearScreen();
 		}
