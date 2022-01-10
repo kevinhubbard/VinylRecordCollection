dd355d54423eca75d65537c38988f73665f04352
Kevin H <dgkallday98@gmail.com>
2022-01-10 13:04:25 -0500

    added a link to website

--- PanelBottom.java
+++ PanelBottom.java
@@ -1,11 +1,40 @@
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
+import java.net.*;
 
-public class PanelBottom extends JPanel {
+import java.awt.Desktop;
+import java.io.*;
+
+public class PanelBottom extends JPanel{
+
+
+		Desktop desktop = java.awt.Desktop.getDesktop();
+
 	public PanelBottom() {
-		JLabel end = new JLabel("Kevin Jr LLC");
+
+
+		JLabel end = new JLabel("KevinJr.net");
 		setBackground(Color.GRAY);
+		end.addMouseListener(new OpenLink());
 		add(end);
 	}
 
+
+	public class OpenLink implements MouseListener {
+		public void mouseClicked(MouseEvent ev) {
+			try {
+				URI uri = new URI("http://www.kevinjr.net");
+				desktop.browse(uri);
+			} catch (Exception e) {e.printStackTrace();}
+		}
+		public void mouseEntered(MouseEvent ev) {
+			setCursor(new Cursor(Cursor.HAND_CURSOR));
+		}
+		public void mouseExited(MouseEvent ev) {
+			setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+		}
+		public void mousePressed(MouseEvent ev) {}
+		public void mouseReleased(MouseEvent ev) {}
+	}
 }
