10a5f398351700ad4d838d66eea8336f274bbab8
Kevin H <dgkallday98@gmail.com>
2022-01-15 19:42:16 -0500

    gui updates

--- Main.java
+++ Main.java
@@ -17,11 +17,13 @@ public class Main {
 		PanelTop pTop = new PanelTop();
 		PanelMain pMain = new PanelMain();
 		PanelBottom pBot = new PanelBottom();
+		PanelRight pRight = new PanelRight();
 		MyMenuBar bar = new MyMenuBar();
 		frame.setJMenuBar(bar);
 		frame.getContentPane().add(BorderLayout.NORTH, pTop);
 		frame.getContentPane().add(BorderLayout.CENTER, pMain);
 		frame.getContentPane().add(BorderLayout.SOUTH, pBot);
+		frame.getContentPane().add(BorderLayout.EAST, pRight);
 		frame.setSize(750, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
